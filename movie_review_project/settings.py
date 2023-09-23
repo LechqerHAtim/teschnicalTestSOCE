d06132b49aee743223c703a8a09d66279ba99aa8
hatim lechqer <hatim.lechqer@gmail.com>
2023-09-23 11:37:05 +0100

    add redoc and swager

--- movie_review_project/settings.py
+++ movie_review_project/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    "movies"
+    "movies",
+      'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- movie_review_project/urls.py
+++ movie_review_project/urls.py
@@ -15,9 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+    openapi.Info(
+        title="myAPI",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.myapp.com/terms/",
+        contact=openapi.Contact(email="hatim.lechqer@gmail.com"),
+        license=openapi.License(name="my License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('movies.urls')),  # Include the urls from the movies app
+      path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
     
 ]
